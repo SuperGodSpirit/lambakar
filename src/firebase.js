428137815542af0d65f1a386d55bca551c0f7845
SuperGodSpirit <premkumar190706@gmail.com>
2026-05-08 13:18:53 +0530

    security: move Firebase config to environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "REDACTED",
-  authDomain: "lamba-kar.firebaseapp.com",
-  projectId: "lamba-kar",
-  storageBucket: "lamba-kar.firebasestorage.app",
-  messagingSenderId: "71467806447",
-  appId: "1:71467806447:web:5db0535dcbbc9dcc9a351a",
-  measurementId: "G-CNRPXY8BVT"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
